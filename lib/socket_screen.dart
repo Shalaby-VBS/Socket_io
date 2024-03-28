f67a3db0cfb2ec0205cfa478e2e2cfb4fe5a6b05
Shalaby-VBS <shalaby.vbs@gmail.com>
2024-03-28 11:23:06 +0200

    Shalaby-VBS

--- lib/socket_screen.dart
+++ lib/socket_screen.dart
@@ -12,6 +12,7 @@ class _SocketScreenState extends State<SocketScreen> {
   late Socket socket;
 
   TextEditingController messageController = TextEditingController();
+
   List<String> messages = [];
 
   @override
